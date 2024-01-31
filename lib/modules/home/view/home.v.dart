7967cb8c1cd32e05f961fe28da8a8386c4f2843e
Md. Shoeb Adnan <madnan172095@bscse.uiu.ac.bd>
2024-01-31 09:29:32 +0600

    Base Setup

---
+++ lib/modules/home/view/home.v.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeView extends ConsumerWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Search Example'),
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search...',
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 20, // Replace with your actual data length
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Item $index'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
